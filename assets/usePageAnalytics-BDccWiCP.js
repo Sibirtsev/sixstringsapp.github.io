b6480f82f15d3ff8c97624574ebc2f41f2683026
Alexey Sibirtsev <alexey.sibirtsev@gmail.com>
2026-02-12 11:44:11 +0100

    metronome favorites

---
+++ assets/usePageAnalytics-BDccWiCP.js
@@ -0,0 +1,1 @@
+import{d as e,r}from"./index-0v6o6Bec.js";function o(t,s={}){const{tracker:a}=e.useAnalytics();r.useEffect(()=>{try{a.trackEvent("page_open",{page:t,...s})}catch{}},[])}export{o as u};
