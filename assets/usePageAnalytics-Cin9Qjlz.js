e758607a4d92c2c1c19b34b64b70bfe00c73b45a
Alexey Sibirtsev <alexey.sibirtsev@gmail.com>
2026-02-14 18:08:08 +0100

    song: fragment and loop

--- assets/usePageAnalytics-Cin9Qjlz.js
+++
@@ -1,1 +0,0 @@
-import{d as e,r}from"./index-YOfq9U9C.js";function o(t,s={}){const{tracker:a}=e.useAnalytics();r.useEffect(()=>{try{a.trackEvent("page_open",{page:t,...s})}catch{}},[])}export{o as u};
