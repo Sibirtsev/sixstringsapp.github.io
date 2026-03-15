fe6259424359e384f5215746331b08be4c4a1cbb
Alexey Sibirtsev <alexey.sibirtsev@gmail.com>
2026-03-15 19:02:13 +0100

    random key selector

---
+++ assets/usePageAnalytics-DvLZnMRn.js
@@ -0,0 +1,1 @@
+import{d as e,r}from"./index-6E9MCP-2.js";function o(t,s={}){const{tracker:a}=e.useAnalytics();r.useEffect(()=>{try{a.trackEvent("page_open",{page:t,...s})}catch{}},[])}export{o as u};
