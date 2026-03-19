75d631c38a57f9c38e1c6134e6c5036bc496b3cb
Alexey Sibirtsev <alexey.sibirtsev@gmail.com>
2026-03-20 00:21:37 +0100

    chord functions trainer

---
+++ assets/usePageAnalytics-DRMyM2eu.js
@@ -0,0 +1,1 @@
+import{d as e,r}from"./index-C_4uTgjn.js";function o(t,s={}){const{tracker:a}=e.useAnalytics();r.useEffect(()=>{try{a.trackEvent("page_open",{page:t,...s})}catch{}},[])}export{o as u};
--- assets/usePageAnalytics-DvLZnMRn.js
+++
@@ -1,1 +0,0 @@
-import{d as e,r}from"./index-6E9MCP-2.js";function o(t,s={}){const{tracker:a}=e.useAnalytics();r.useEffect(()=>{try{a.trackEvent("page_open",{page:t,...s})}catch{}},[])}export{o as u};
