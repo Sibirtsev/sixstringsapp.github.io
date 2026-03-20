47402a5fbdf158b75eebdb5871bd4cf41292fe8a
Alexey Sibirtsev <alexey.sibirtsev@gmail.com>
2026-03-20 16:21:38 +0100

    interval trainer

--- assets/usePageAnalytics-D4ezMREr.js
+++
@@ -1,1 +0,0 @@
-import{d as e,r}from"./index-BcaJ7Go5.js";function o(t,s={}){const{tracker:a}=e.useAnalytics();r.useEffect(()=>{try{a.trackEvent("page_open",{page:t,...s})}catch{}},[])}export{o as u};
