5fb75453f022aed58f2a5ab171be806f63124294
Alexey Sibirtsev <alexey.sibirtsev@gmail.com>
2026-03-24 11:53:52 +0100

    harmony page improvements

--- assets/usePageAnalytics-Bs6pojPh.js
+++
@@ -1,1 +0,0 @@
-import{d as e,r}from"./index-CM138xhW.js";function o(t,s={}){const{tracker:a}=e.useAnalytics();r.useEffect(()=>{try{a.trackEvent("page_open",{page:t,...s})}catch{}},[])}export{o as u};
