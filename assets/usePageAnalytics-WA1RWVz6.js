d1758ae69e8862dda82ce599c2cb848e103a0464
Alexey Sibirtsev <alexey.sibirtsev@gmail.com>
2026-03-30 16:05:46 +0200

    seo

---
+++ assets/usePageAnalytics-WA1RWVz6.js
@@ -0,0 +1,1 @@
+import{d as e,r}from"./index-DGSG-5tG.js";function o(t,s={}){const{tracker:a}=e.useAnalytics();r.useEffect(()=>{try{a.trackEvent("page_open",{page:t,...s})}catch{}},[])}export{o as u};
