e0f2aeabdd1c9ff02037c0e382763ccbd6177ec5
Alexey Sibirtsev <alexey.sibirtsev@gmail.com>
2026-03-31 12:48:14 +0200

    blog and articles

---
+++ assets/usePageAnalytics-CICuPaA6.js
@@ -0,0 +1,1 @@
+import{C as e,r}from"./index--8SAAuf4.js";function o(t,s={}){const{tracker:a}=e.useAnalytics();r.useEffect(()=>{try{a.trackEvent("page_open",{page:t,...s})}catch{}},[])}export{o as u};
