844fa82f2538759fa408e10751f91893b7e2e2fe
Alexey Sibirtsev <alexey.sibirtsev@gmail.com>
2026-04-06 17:06:00 +0200

    new article: chords

---
+++ assets/usePageAnalytics-CDzmj6Zu.js
@@ -0,0 +1,1 @@
+import{C as e,r}from"./index-WezguexA.js";function o(t,s={}){const{tracker:a}=e.useAnalytics();r.useEffect(()=>{try{a.trackEvent("page_open",{page:t,...s})}catch{}},[])}export{o as u};
--- assets/usePageAnalytics-CLRppxsY.js
+++
@@ -1,1 +0,0 @@
-import{C as e,r}from"./index-bw30zlU5.js";function o(t,s={}){const{tracker:a}=e.useAnalytics();r.useEffect(()=>{try{a.trackEvent("page_open",{page:t,...s})}catch{}},[])}export{o as u};
