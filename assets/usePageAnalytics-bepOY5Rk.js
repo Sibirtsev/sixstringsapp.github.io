0f392cc7b01d7d8c6652ac62d4438a9d5abd8c97
Alexey Sibirtsev <alexey.sibirtsev@gmail.com>
2026-04-06 18:49:30 +0200

    feat: caged updates

---
+++ assets/usePageAnalytics-bepOY5Rk.js
@@ -0,0 +1,1 @@
+import{C as e,r}from"./index-8xas9NSu.js";function o(t,s={}){const{tracker:a}=e.useAnalytics();r.useEffect(()=>{try{a.trackEvent("page_open",{page:t,...s})}catch{}},[])}export{o as u};
