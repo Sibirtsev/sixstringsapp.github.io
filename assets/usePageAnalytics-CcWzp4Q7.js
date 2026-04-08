7430bfff70f1499c7b7043d4b06211a009e76c00
Alexey Sibirtsev <alexey.sibirtsev@gmail.com>
2026-04-08 11:57:43 +0200

    feat: cannonical urls

---
+++ assets/usePageAnalytics-CcWzp4Q7.js
@@ -0,0 +1,1 @@
+import{C as e,r}from"./index-szKKyGCc.js";function o(t,s={}){const{tracker:a}=e.useAnalytics();r.useEffect(()=>{try{a.trackEvent("page_open",{page:t,...s})}catch{}},[])}export{o as u};
--- assets/usePageAnalytics-bepOY5Rk.js
+++
@@ -1,1 +0,0 @@
-import{C as e,r}from"./index-8xas9NSu.js";function o(t,s={}){const{tracker:a}=e.useAnalytics();r.useEffect(()=>{try{a.trackEvent("page_open",{page:t,...s})}catch{}},[])}export{o as u};
