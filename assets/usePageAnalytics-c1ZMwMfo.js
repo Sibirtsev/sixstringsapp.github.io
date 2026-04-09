7463d91f6fc65ddae9628fbf4346bdfe8779fb67
Alexey Sibirtsev <alexey.sibirtsev@gmail.com>
2026-04-09 12:46:27 +0200

    article: harmony

--- assets/usePageAnalytics-c1ZMwMfo.js
+++
@@ -1,1 +0,0 @@
-import{C as e,r}from"./index-CXtbP2PW.js";function o(t,s={}){const{tracker:a}=e.useAnalytics();r.useEffect(()=>{try{a.trackEvent("page_open",{page:t,...s})}catch{}},[])}export{o as u};
