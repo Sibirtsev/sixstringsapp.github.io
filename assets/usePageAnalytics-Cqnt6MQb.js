5758b2ce31737c68231f1c040637a8febbb01e03
Alexey Sibirtsev <alexey.sibirtsev@gmail.com>
2026-04-13 11:59:20 +0200

    article: metronome

--- assets/usePageAnalytics-Cqnt6MQb.js
+++
@@ -1,1 +0,0 @@
-import{E as e,r}from"./index-CxTQjYeH.js";function o(t,s={}){const{tracker:a}=e.useAnalytics();r.useEffect(()=>{try{a.trackEvent("page_open",{page:t,...s})}catch{}},[])}export{o as u};
---
+++ assets/usePageAnalytics-dGL4kM0e.js
@@ -0,0 +1,1 @@
+import{E as e,r}from"./index-DiqEi6T6.js";function o(t,s={}){const{tracker:a}=e.useAnalytics();r.useEffect(()=>{try{a.trackEvent("page_open",{page:t,...s})}catch{}},[])}export{o as u};
