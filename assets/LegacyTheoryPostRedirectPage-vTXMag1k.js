7a0126d5dd9c76e4d609bbdba2563d56e315095f
Alexey Sibirtsev <alexey.sibirtsev@gmail.com>
2026-05-02 11:27:01 +0200

    feat: chord collections and diagram downloads

---
+++ assets/LegacyTheoryPostRedirectPage-vTXMag1k.js
@@ -0,0 +1,1 @@
+import{l as c,c as n,j as e,N as r,w as a,D as o,B as i}from"./index-CRM5v0aC.js";const g=()=>{const{search:u}=c(),{slug:t}=n();if(!t)return e.jsx(r,{to:a("/theory",o),replace:!0});const s=i(t);return s?e.jsx(r,{to:`${s}${u}`,replace:!0}):e.jsx(r,{to:a("/404",o),replace:!0})};export{g as default};
