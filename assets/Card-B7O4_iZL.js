6376dfa65fb25d089d0b39f85aa9e09a3e53e9e4
Alexey Sibirtsev <alexey.sibirtsev@gmail.com>
2026-05-29 07:39:53 +0200

    new article and arpeggio generators

---
+++ assets/Card-B7O4_iZL.js
@@ -0,0 +1,1 @@
+import{j as i,W as l,X as t}from"./index-CbpOTFWG.js";const m=({children:a,className:s="",padding:r="md",shadow:o="md",hover:d=!1})=>{const e=["ui-card",l[r],t[o],d?"is-hoverable":"",s].filter(Boolean).join(" ");return i.jsx("div",{className:e,children:a})};export{m as C};
