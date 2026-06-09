16593f4556f8a7cbd73f8158378d1a3672668407
Alexey Sibirtsev <alexey.sibirtsev@gmail.com>
2026-06-09 11:44:13 +0200

    article: musical ear

--- assets/Card-B5Y1uKmB.js
+++
@@ -1,1 +0,0 @@
-import{j as i,V as l,W as t}from"./index-DljRMepd.js";const m=({children:a,className:s="",padding:r="md",shadow:o="md",hover:d=!1})=>{const e=["ui-card",l[r],t[o],d?"is-hoverable":"",s].filter(Boolean).join(" ");return i.jsx("div",{className:e,children:a})};export{m as C};
